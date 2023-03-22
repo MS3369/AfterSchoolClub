e02ded6f73defd1f9084d2bcd86443a81c02b5ea
Shahz <ms3369@live.mdx.ac.uk>
2023-03-23 00:10:44 +0400

    Html

---
+++ lessons.js
@@ -0,0 +1,116 @@
+let lessons = [
+    {
+        "id" : 001,
+        "image" :'img/swimming.jpg',
+        "subject" : 'Swimming',
+        "location": 'Townhall',
+        "price" : 220,
+        "detail": 'this is a beginner swimming class',
+        "availablespace": 5,
+ 
+    },
+
+    {
+    
+        "id" : 101,
+        "image" :'img/Archery.png',
+        "subject" : 'Archery',
+        "location": 'City Centre',
+        "price" : 350,
+        "detail": 'this is a beginner Archery training',
+        "availablespace": 5,
+        
+    },
+
+    {
+        
+        "id" : 202,
+        "image" :'img/history.jpg',
+        "subject" : 'History',
+        "location": 'Outskirts city',
+        "price" : 290,
+        "detail": 'this is a beginner history class',
+        "availablespace": 5,
+      
+     },
+
+    {
+        
+        "id" : 303,
+        "image" :'img/english.png',
+        "subject" : 'English',
+        "location": 'Downtown',
+        "price" : 240,
+        "detail": 'This is an English class for Non-English students',
+        "availablespace": 5,
+     },
+
+    {
+        
+        "id" : 404,
+        "image" :'img/art.jpg',
+        "subject" : 'Arts & Drawing',
+        "location": 'Market Area',
+        "price" : 310,
+        "detail": 'This is an Arts & Drawing class',
+        "availablespace": 5,
+
+     },
+
+     {
+        
+        "id" : 505,
+        "image" :'img/computer.jpg',
+        "subject" : 'Computing',
+        "location": 'Beach Area',
+        "price" : 460,
+        "detail": 'Computing Introduction course',
+        "availablespace": 5,
+
+     },
+    
+    {
+        
+        "id" : 606,
+        "image" :'img/Culinary.jpg',
+        "subject" : 'Culinary',
+        "location": 'Townhall',
+        "price" : 190,
+        "detail": 'Culinary starter course',
+        "availablespace": 5,
+
+     },
+
+    {
+        
+        "id" : 707,
+        "image" :'img/photography.jpg',
+        "subject" : 'Photography',
+        "location": 'Outskirts city',
+        "price" : 310,
+        "detail": 'Photography class for kids',
+        "availablespace": 5,
+
+     }, 
+    {
+        
+        "id" : 808,
+        "image" :'img/baking.jpg',
+        "subject" : 'Baking',
+        "location": 'Market Area',
+        "price" : 250,
+        "detail": 'Baking class for beginners',
+        "availablespace": 5,
+ 
+     },
+     {
+        
+        "id" : 909,
+        "image" :'img/french.jpg',
+        "subject" : 'French',
+        "location": 'City Centre',
+        "price" : 310,
+        "detail": 'Learning French as a second language',
+        "availablespace": 5,
+      },
+]
