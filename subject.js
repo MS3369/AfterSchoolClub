d853bf27051fd809322801fc497df639d3175685
Shahz <ms3369@live.mdx.ac.uk>
2023-03-23 02:41:44 +0400

    final search

---
+++ subject.js
@@ -0,0 +1,86 @@
+let   subjects  = [
+    {
+        courses: "Photography",
+        location: 'Beach View',
+        price: "500",
+        detail: 'Photography class for kids',
+        availableSpace: 5,
+        image: "img/photography.jpg"
+    }, 
+    {
+        courses: "Swimming",
+        location: 'Marina Beach',
+        price: "1200",
+        "detail": 'this is a beginner swimming class',
+        availableSpace: 5,
+        image: "img/swimming.jpg"
+    },
+     {
+        courses: "Archery",
+        location: 'Al Ain',
+        price: "2000",
+        "detail": 'Archery training for beginners',
+        availableSpace: 5,
+        image: "img/archery.png"
+    },
+     {
+        courses: "French Language",
+        location: 'Knowledge Park',
+        price: "250",
+        "detail": 'Learning French as a second language',
+        availableSpace: 5,
+        image: "img/french.jpg"
+    },
+     {
+        courses: "Baking",
+        location: 'City Centre',
+        price: "400",
+        "detail": 'Baking class for beginners',
+        availableSpace: 5,
+        image: "img/baking.jpg"
+    },
+     {
+        courses: "Art & Craft",
+        location: 'Townhall',
+        price: "1100",
+        "detail": 'This is an Arts & Drawing class',
+        availableSpace: 5,
+        image: "img/art.jpg"
+    },
+     {
+        courses: "Computer",
+        location: 'Downtown',
+        price: "1500",
+        "detail": 'Computing Introduction course',
+        availableSpace: 5,
+        image: "img/computer.jpg"
+    },
+     {
+        courses: "Culinary",
+        location: 'Palm Resort',
+        price: "2400",
+        "detail": 'Culinary starter course',
+        availableSpace: 5,
+        image: "img/culinary.jpg"
+    },
+     {
+        courses: "English",
+        location: 'Business Bay',
+        price: "500",
+        "detail": 'This is an English class for Non-English students',
+        availableSpace: 5,
+        image: "img/english.png"
+
+    },
+     {
+        courses: "History",
+        location: 'Deira',
+        price: "250",
+        "detail": 'this is a beginner history class',
+        availableSpace: 5,
+        image: "img/History.jpg"
+
+    },
+ ]
+   
+    
